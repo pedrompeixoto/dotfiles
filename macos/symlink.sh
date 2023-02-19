c52d84f2553875f533a8ad6f13a293f7468eb90e
pedrompeixoto <pmpeixoto@protonmail.com>
2023-02-19 18:37:38 +0000

    Git config and macos symlink script

---
+++ macos/symlink.sh
@@ -0,0 +1,15 @@
+dotfiles_folder=$HOME"/projects/dotfiles"
+
+programs=()
+
+programs+=(git)
+source_git=$dotfiles_folder/common/git/gitconfig
+target_git=$HOME/.gitconfig
+
+for program in ${programs[@]}
+do
+	source=source_${program}
+	target=target_${program}
+	ln -sfhF ${!source} ${!target}
+	echo "Symlinked " $program " " ${!source} " -> " ${!target}
+done
