519e63ae46619513c7fc801604ea2b6d49deadc6
pedrompeixoto <pmpeixoto@protonmail.com>
2023-02-20 23:34:19 +0000

    add ripgrep to fedora dev essetials install script

--- fedora/install_dev_essentials.sh
+++ fedora/install_dev_essentials.sh
@@ -4,4 +4,5 @@ sudo rpm --import https://dl.google.com/linux/linux_signing_key.pub
 sudo dnf install -y neovim \
 	https://github.com/wez/wezterm/releases/download/20221119-145034-49b9839f/wezterm-20221119_145034_49b9839f-1.fedora36.x86_64.rpm \
 	https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm \
-	i3 i3status dmenu
+	i3 i3status dmenu \
+    ripgrep
