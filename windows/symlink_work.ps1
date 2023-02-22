989b46274a5f04a84eec28a14b9c42b98a2da018
pedrompeixoto <pmpeixoto@protonmail.com>
2023-02-22 15:23:51 +0000

    Fix neovim path

--- windows/symlink_work.ps1
+++ windows/symlink_work.ps1
@@ -1,6 +1,6 @@
 $dotfilesFolder= "$env:USERPROFILE\dotfiles"
 
-New-Item -ItemType SymbolicLink -Force -Path $env:LOCALAPPDATA\nvim -Target $dotfilesFolder\common\neovim
+New-Item -ItemType SymbolicLink -Force -Path $env:LOCALAPPDATA\nvim -Target $dotfilesFolder\common\nvim
 
 New-Item -ItemType SymbolicLink -Force -Path $env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 -Target $dotfilesFolder\windows\powershell\profile_work.ps1
 
