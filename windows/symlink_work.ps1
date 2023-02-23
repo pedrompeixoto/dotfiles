7cbe90279dab20ebe17d5e778d3444b507227852
pedrompeixoto <pmpeixoto@protonmail.com>
2023-02-23 21:37:11 +0000

    Refactor symlink_work script to better match mac and linux symlink structure.

--- windows/symlink_work.ps1
+++ windows/symlink_work.ps1
@@ -1,7 +1,24 @@
 $dotfilesFolder= "$env:USERPROFILE\dotfiles"
 
-New-Item -ItemType SymbolicLink -Force -Path $env:LOCALAPPDATA\nvim -Target $dotfilesFolder\common\nvim
+$programs = @(
+    [pscustomobject]@{
+        name = "neovim"
+        source_path = $dotfilesFolder\common\nvim
+        target_path = $env:LOCALAPPDATA\nvim 
+    }
+    [pscustomobject]@{
+        name = "powershell profile"
+        source_path = $dotfilesFolder\windows\powershell\profile_work.ps1
+        target_path = $env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 
+    }
+    [pscustomobject]@{
+        name = "windows terminal"
+        source_path = $dotfilesFolder\windows\windows_terminal\settings.json 
+        target_path = $env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json 
+    }
+)
 
-New-Item -ItemType SymbolicLink -Force -Path $env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 -Target $dotfilesFolder\windows\powershell\profile_work.ps1
-
-New-Item -ItemType SymbolicLink -Force -Path $env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json -Target $dotfilesFolder\windows\windows_terminal\settings.json 
+foreach ($program in $programs) {
+    New-Item -ItemType SymbolicLink -Force -Path $program.target_path -Target $program.source_path
+    Write-Host "Symlinked " + $program.name
+}
