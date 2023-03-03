f42bea2e9d0648da5b034ba3842a4106f41b2c3a
pedrompeixoto <pmpeixoto@protonmail.com>
2023-03-03 11:27:02 +0000

    noevim: Add mappings to indent code without losing focus and to toggle line numbers.

--- common/nvim/lua/pmpp/remaps.lua
+++ common/nvim/lua/pmpp/remaps.lua
@@ -3,3 +3,6 @@ vim.g.mapleader = " "
 local opts = {buffer = bufnr, remap = false}
 vim.keymap.set("v", "<", "<gv", opts)
 vim.keymap.set("v", ">", ">gv", opts)
+vim.keymap.set("i", "jj", "<Esc>", opts)
+vim.keymap.set("n", "<leader>tl", "<cmd>set relativenumber<CR>", opts)
+vim.keymap.set("n", "<leader>tL", "<cmd>set norelativenumber<CR>", opts)
