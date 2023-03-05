6134e372f1798fce4caf2a1cdcf8f7b98bf1e784
pedrompeixoto <pmpeixoto@protonmail.com>
2023-03-05 17:57:41 +0000

    enable bluetooth on install_wm script

--- arch/install_wm.sh
+++ arch/install_wm.sh
@@ -1,6 +1,6 @@
-sudo pacman -Syu xorg-server xorg-xinit awesome xterm nvidia pipewire pipewire-pulse pavucontrol lwd
-
+sudo pacman -Syu xorg-server xorg-xinit awesome xterm nvidia pipewire pipewire-pulse pavucontrol iwd bluez bluez-utils
 
 sudo systemctl enable --now systemd-resolved
 sudo systemctl enable --now iwd
+sudo systemctl enable --now bluetooth
 
