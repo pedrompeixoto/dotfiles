95e74848926045a2d25d5a79775340b44c35b7d8
pedrompeixoto <pmpeixoto@protonmail.com>
2023-04-07 22:13:52 +0100

    Add awesome to configs and implement keyboard layout switching

--- arch/install_tools.sh
+++ arch/install_tools.sh
@@ -1,3 +1,5 @@
-sudo pacman -Syu neovim wezterm firefox git openssh spotify-launcher discord gcc
+sudo pacman -Syu neovim wezterm firefox git openssh spotify-launcher discord gcc xclip
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 
 git clone --depth 1 https://github.com/wbthomason/packer.nvim  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
--- arch/symlink.sh
+++ arch/symlink.sh
@@ -15,6 +15,10 @@ programs+=(wezterm)
 source_wezterm=$config_folder/wezterm
 target_wezterm=$HOME/.config/wezterm
 
+programs+=(awesomewm)
+source_awesomewm=$config_folder/awesome
+target_awesomewm=$HOME/.config/awesome
+
 for program in ${programs[@]}
 do
 	source=source_${program}
