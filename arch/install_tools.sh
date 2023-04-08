eae0f90a47257c32f3362bdf7610c8ec215ba1cd
pedrompeixoto <pmpeixoto@protonmail.com>
2023-04-08 12:58:13 +0100

    Add 1password to install_tools script on arch linux

--- arch/install_tools.sh
+++ arch/install_tools.sh
@@ -3,3 +3,9 @@ sudo pacman -Syu neovim wezterm firefox git openssh spotify-launcher discord gcc
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 
 git clone --depth 1 https://github.com/wbthomason/packer.nvim  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+git clone https://aur.archlinux.org/1password.git
+cd 1password
+makepkg -si
+cd ..
+rm -rf 1password
