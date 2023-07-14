88f2276d6fc52964bc717692d165d4a9356b19ab
pedrompeixoto <pmpeixoto@protonmail.com>
2023-07-14 22:12:15 +0100

    neovim jj to esc

--- configs/nvimV2/init.lua
+++ configs/nvimV2/init.lua
@@ -216,6 +216,9 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
+-- Esc jj
+vim.keymap.set('i', 'jj', '<Esc>', { silent = true })
+
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
