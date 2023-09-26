8105d1ea869553747f0cfd9f62ea6ced1d1e6006
pedrompeixoto <pmpeixoto@protonmail.com>
2023-09-26 10:06:34 +0100

    nvim: Astro lsp and treesitter

--- configs/nvimV2/init.lua
+++ configs/nvimV2/init.lua
@@ -268,7 +268,7 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
 	-- Add languages to be installed here that you want installed for treesitter
-	ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'tsx', 'typescript', 'vimdoc', 'vim' },
+	ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'tsx', 'typescript', 'vimdoc', 'vim', 'astro', 'css' },
 
 	-- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
 	auto_install = false,
@@ -390,7 +390,7 @@ local servers = {
 	-- clangd = {},
 	-- gopls = {},
 	-- pyright = {},
-	-- rust_analyzer = {},
+	astro = {},
 	tsserver = {},
 
 	lua_ls = {
