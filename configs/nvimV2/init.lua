f9a6a79dcc3b8b518e14897aca72a03d711a268a
pedrompeixoto <pmpeixoto@protonmail.com>
2023-10-09 15:42:56 +0100

    Neovim: set fidget tag to legacy and update indent-blankline to match new version setup.

--- configs/nvimV2/init.lua
+++ configs/nvimV2/init.lua
@@ -47,6 +47,7 @@ require('lazy').setup({
 
 			-- Useful status updates for LSP
 			-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+			{ 'j-hui/fidget.nvim', tag = "legacy", opts = {} },
 			{ 'j-hui/fidget.nvim', opts = {} },
 
 			-- Additional lua configuration, makes nvim stuff amazing!
@@ -105,16 +106,7 @@ require('lazy').setup({
 		},
 	},
 
-	{
-		-- Add indentation guides even on blank lines
-		'lukas-reineke/indent-blankline.nvim',
-		-- Enable `lukas-reineke/indent-blankline.nvim`
-		-- See `:help indent_blankline.txt`
-		opts = {
-			char = '┊',
-			show_trailing_blankline_indent = false,
-		},
-	},
+	{ "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
 
 	-- "gc" to comment visual regions/lines
 	{ 'numToStr/Comment.nvim', opts = {} },
