97232614fc93d32499be3469b14b573be4234da1
pedrompeixoto <pmpeixoto@protonmail.com>
2024-01-07 01:02:06 +0000

    neovim config revamp options related to ui

--- configs/nvim/lua/pedrompeixoto/options.lua
+++ configs/nvim/lua/pedrompeixoto/options.lua
@@ -3,3 +3,12 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+
+vim.o.hlsearch = false
+
+vim.wo.number = true
+vim.wo.relativenumber = true
+
+vim.o.clipboard = 'unnamedplus'
+
+vim.o.termguicolors = true
