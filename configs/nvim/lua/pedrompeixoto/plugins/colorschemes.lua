44f89c781f3a3ba1ff7d13dea5493aa9ed78dbde
pedrompeixoto <pmpeixoto@protonmail.com>
2024-01-13 12:33:59 +0000

    neovim rose pine colorscheme

--- configs/nvim/lua/pedrompeixoto/plugins/colorschemes.lua
+++ configs/nvim/lua/pedrompeixoto/plugins/colorschemes.lua
@@ -4,7 +4,14 @@ return {
         lazy = false,
         priority = 1000,
         config = function()
-            vim.cmd([[colorscheme tokyonight]])
+            --vim.cmd([[colorscheme tokyonight]])
+        end
+    },
+    { 
+        'rose-pine/neovim',
+        name = 'rose-pine',
+        config = function()
+            vim.cmd([[colorscheme rose-pine]])
         end
     }
 }
