fa05ff6f1632b7bc6e151b68c57ffceff191e695
pedrompeixoto <pmpeixoto@protonmail.com>
2024-01-28 23:55:21 +0000

    tmux macos fix neovim colorshceme

--- macos/symlink.sh
+++ macos/symlink.sh
@@ -12,12 +12,16 @@ source_git=$config_folder/git/gitconfig
 target_git=$HOME/.gitconfig
 
 programs+=(nvim)
-source_nvim=$config_folder/nvimV2
+source_nvim=$config_folder/nvim
 target_nvim=$HOME/.config/nvim
 
 programs+=(wezterm)
 source_wezterm=$config_folder/wezterm
 target_wezterm=$HOME/.config/wezterm
+
+programs+=(tmux)
+source_tmux=$config_folder/tmux
+target_tmux=$HOME/.config/tmux
 
 for program in ${programs[@]}
 do
