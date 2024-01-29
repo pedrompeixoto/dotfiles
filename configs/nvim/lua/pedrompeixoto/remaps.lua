cae966f1d84e9d6a8cf1cd1c4479bcada17ae523
pedrompeixoto <pmpeixoto@protonmail.com>
2024-01-29 00:17:02 +0000

    basic neovim lsp configuration

--- configs/nvim/lua/pedrompeixoto/remaps.lua
+++ configs/nvim/lua/pedrompeixoto/remaps.lua
@@ -1,2 +1,7 @@
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set('i', 'jk', '<Esc>', { silent = true })
+
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
