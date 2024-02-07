bac8a1545c8bb2ea637d3acbadaa1e33f8eac4c4
pedrompeixoto <pmpeixoto@protonmail.com>
2024-02-07 22:07:52 +0000

    Neovim toggle relative line numbers and add a statusbar

--- configs/nvim/lua/pedrompeixoto/remaps.lua
+++ configs/nvim/lua/pedrompeixoto/remaps.lua
@@ -1,3 +1,5 @@
+require('pedrompeixoto.utils')
+
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set('i', 'jk', '<Esc>', { silent = true })
 
@@ -5,3 +7,5 @@ vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
+vim.keymap.set('n', '<leader>ln', ':lua ToggleLineNumbers()<CR>', { noremap = true, silent = true })
