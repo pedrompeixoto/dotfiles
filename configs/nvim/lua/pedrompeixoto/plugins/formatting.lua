e5f890e105f8601757c050f4a4c401bb0c2643da
pedrompeixoto <pmpeixoto@protonmail.com>
2024-02-09 20:18:24 +0000

    Neovim neoformat keymap

--- configs/nvim/lua/pedrompeixoto/plugins/formatting.lua
+++ configs/nvim/lua/pedrompeixoto/plugins/formatting.lua
@@ -3,5 +3,6 @@ return {
 
     config = function()
         vim.g.neoformat_try_node_exe = 1
+        vim.keymap.set('n', '<leader>F', ':Neoformat <CR>', { noremap = true, silent = true })
     end
 }
