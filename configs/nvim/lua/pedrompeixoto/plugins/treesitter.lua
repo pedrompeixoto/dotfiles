a7a02bf772c38973d9cdf25bc8e173de6987dc98
pedrompeixoto <pmpeixoto@protonmail.com>
2024-02-09 22:47:47 +0000

    Neovim treesitter fixes and add completion via cmp

--- configs/nvim/lua/pedrompeixoto/plugins/treesitter.lua
+++ configs/nvim/lua/pedrompeixoto/plugins/treesitter.lua
@@ -5,7 +5,11 @@ return {
         require("nvim-treesitter.configs").setup({
             -- A list of parser names, or "all"
             ensure_installed = {
-                "javascript", "typescript", "c", "lua", "astro",
+                "javascript", "typescript", "c", "lua", "astro", 'css', 'tsx'
+            },
+
+            highlight = {
+                enable = true,
             },
         })
     end
