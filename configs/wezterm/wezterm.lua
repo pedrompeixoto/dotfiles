a7e2e83d3329727806478a6b9ccbfe8fc2ae1e44
pedrompeixoto <pmpeixoto@protonmail.com>
2025-07-21 10:41:07 +0100

    Change wezterm font

--- configs/wezterm/wezterm.lua
+++ configs/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 
 return {
-    font = wezterm.font("Liberation Mono"),
+    font = wezterm.font("JetBrainsMono Nerd Font"),
     font_size = 14.0,
     color_scheme = "tokyonight_night",
 }
