ebbce0fec07ae5337601c4141dde5656e9fc11bf
pedrompeixoto <pmpeixoto@protonmail.com>
2025-07-27 21:54:21 +0100

    Remove font ligatures wezterm

--- configs/wezterm/wezterm.lua
+++ configs/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 
 return {
-    font = wezterm.font("JetBrainsMono Nerd Font"),
+    font = wezterm.font("JetBrainsMonoNL Nerd Font"),
     font_size = 14.0,
     color_scheme = "tokyonight_night",
 }
