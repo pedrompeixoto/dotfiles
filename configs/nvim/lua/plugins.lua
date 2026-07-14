e1fdc12546df7bf1b4868169e6af9112a23a15b3
Pedro Peixoto <pmpeixoto@protonmail.com>
2026-07-15 00:22:14 +0100

    nvim: add keymap to toggle neocodeium on/off

--- configs/nvim/lua/plugins.lua
+++ configs/nvim/lua/plugins.lua
@@ -78,6 +78,7 @@ require("lazy").setup({
             keymap.set("i", "<A-r>", function() neocodeium.cycle_or_complete(-1) end,
                 { desc = "Cycle AI suggestions (reverse)" })
             keymap.set("i", "<A-c>", neocodeium.clear, { desc = "Clear AI suggestion" })
+            keymap.set("n", "<leader>at", neocodeium.toggle, { desc = "Toggle AI suggestions" })
         end,
     },
     {
