a1410ad898af4b3f899a3c8514b399032d2a6643
Pedro Peixoto <pmpeixoto@protonmail.com>
2026-07-21 00:57:17 +0100

    Completion fix and ghosty theme

--- configs/nvim/lua/setup/completion.lua
+++ configs/nvim/lua/setup/completion.lua
@@ -10,33 +10,11 @@ function M.blink()
         sources = {
             default = { "lsp", "path", "snippets", "buffer" },
         },
-        completion = {
-            menu = {
-                -- keep the menu closed by default so NeoCodeium's ghost text
-                -- isn't fighting the cmp popup for screen space
-                auto_show = function(ctx)
-                    return ctx.mode ~= "default"
-                end,
-            },
-        },
     })
 end
 
 function M.neocodeium()
-    local neocodeium = require("neocodeium")
-    local blink = require("blink.cmp")
-
-    neocodeium.setup({
-        filter = function()
-            return not blink.is_visible()
-        end,
-    })
-
-    -- close the AI ghost text as soon as the cmp menu takes over
-    vim.api.nvim_create_autocmd("User", {
-        pattern = "BlinkCmpMenuOpen",
-        callback = function() neocodeium.clear() end,
-    })
+    require("neocodeium").setup()
 end
 
 return M
